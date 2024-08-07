2eb6c6fb7e17f2fbc98928fe62cc2042b0aa6156
Giri <guppuganti@gmail.com>
2024-08-07 17:25:26 +0000

    python updated

--- python/script.py
+++ python/script.py
@@ -79,3 +79,47 @@ print(max(list_num))
 print(min(list_num))
 
 print(list_num[1] * list_num[3])
+
+
+
+
+##################################
+#Dictionary
+
+
+students = {'bob':12, 'john':15, 'emily':20}
+print(students['bob'])
+print(len(students))
+print(students)
+
+
+####Tuples
+## Tuples are immutable; can't be modified after creation
+
+tup1= ('oranges', 'apples', 'bananas')
+tup2=(12,14)
+tup3=tup1 +tup2 
+print (tup1)
+print (tup2)
+print (tup3)
+
+##############################
+#conditional statement
+
+if 5 > 8 :
+     print ("big")
+else:
+     print ("small")
+
+age=45
+
+if age < 20 :
+    print ("you are child")
+elif age > 20 and age < 25:
+    print ("collect student")
+elif age > 20 and age < 40:
+    print ("middle age")
+else:
+    print ("you are old")
+
+
