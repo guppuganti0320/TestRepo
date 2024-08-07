6ee9e398df61e6b014edc116f088e0d70057f356
EC2 Default User <ec2-user@ip-172-31-25-215.us-west-2.compute.internal>
2024-08-07 16:50:06 +0000

    python

---
+++ python/script.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+
+#varibales
+age1 = 10
+age2 = 15
+total = age1 + age2
+print(age1 + age2)
+print (total)
+
+str1 = " sunny day "
+print (str1)
+
+
+fname = "giri"
+lname = "uppuganti"
+print(fname + " " + lname)
+
+
+str2="today is sunny day"
+print(str2[0:5])
+
+##String operations
+
+name ="jake"
+sentense="%s is 15 years old"
+age = "50"
+
+
+## operators
+print(age)
+
+sarah = mike = 15
+
+print (sarah)
+
+name, age = "scott", 25
+print (name)
+print (age)
+
+
+###Placeholders
+
+x = 10
+y = 15
+
+print(f"sum of x and ys is {x + y}")
+
+
+
+### List - data structures
+
+items=['apples','bananas','oranges']
+print(items[0])
+
+
+print(f"{items[0]} good for health")
+
+print(f"first 2 items {items[0:2]}")
+
+## lists are mutable
+
+items.append('berries')
+print (items)
+
+items[0]="cherries"
+print (items)
+del items[2]
+
+print (items)
+
+
+print(f"length of list {len(items)}")
+
+
+
+list_num=[5,2,10,25,11]
+print(max(list_num))
+print(min(list_num))
